82128267c1a8fc00571e666fc17f4d2de5b4490c
David <David.N.Huang@gmail.com>
2020-12-13 20:53:00 -0800

    Complete SwiftUI Tutorial - Moving on

---
+++ SwiftUI Tutorial/My Files/Lesson 8/StartingPoint/Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,30 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by David N. Huang on 12/13/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3/2, contentMode: .fit)
+    }
+}
