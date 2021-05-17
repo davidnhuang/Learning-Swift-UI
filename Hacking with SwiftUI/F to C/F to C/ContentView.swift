d7039054875f0a9903536cb1e8c4061ac1e59bbf
David <david.n.huang@gmail.com>
2021-05-16 22:50:11 -0700

    Continue challenge project

--- Hacking with SwiftUI/F to C/F to C/ContentView.swift
+++ Hacking with SwiftUI/F to C/F to C/ContentView.swift
@@ -7,24 +7,45 @@
 
 import SwiftUI
 
+// Textfield manager
+class textInputManager: ObservableObject {
+    var charLimit = 15
+    
+    @Published var inputValue = "" {
+        didSet {
+            if inputValue.count > charLimit {
+                inputValue = String(inputValue.prefix(charLimit))
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     
-    @State private var startingUnitInput = "" // selecting unit type from an array
+    @State private var startingUnitInput = textInputManager() // selecting unit type from an array
+    @State private var targetConvertUnit = 0
     
-    var unitLabels = ["C˚", "F˚", "K"]
+    var unitTypes = ["C˚", "K", "F˚"]
     
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Convert from")) {
-                    TextField("Temperature in Fahrenheit", text: $startingUnitInput)
+                Section(header: Text("From")) {
+                    TextField("F˚", text: $startingUnitInput.inputValue)
                     // Picker
                 }
                 Section(header: Text("To")) {
-                    // Picker
+                    Picker("Unit", selection: $targetConvertUnit) {
+                        ForEach(0 ..< unitTypes.count) {
+                            Text("\(self.unitTypes[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    Text("Converted here")
+                        .font(.body)
+                        .fontWeight(.regular)
                 }
             }
-            .navigationTitle(Text("F˚→ \(unitLabels[0])"))
+            .navigationTitle(Text("F˚→ \(unitTypes[0])"))
         }
     }
 }
