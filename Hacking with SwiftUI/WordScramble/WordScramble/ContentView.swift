bc2821d763703e8602bf0876567a75ddd57acae6
David <david.n.huang@gmail.com>
2021-07-09 23:13:31 -0700

    day 17

--- Hacking with SwiftUI/WordScramble/WordScramble/ContentView.swift
+++ Hacking with SwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -8,39 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        
-        // Spellchecking
-        let word = "word"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let noMistake = misspelledRange.location == NSNotFound
-        
         NavigationView {
-            List {
-                ForEach(0..<6) {
-                    Section(header: Text("Section \($0+1)")) {
-                        ForEach(0..<5) {
-                            Text("Item \($0)")
-                        }
-                    }
-                }
+            VStack {
+                // Text Input
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
                 
-//                Section(header: Text("Special Section")) {
-//                    // Special section that loads only when file is found
-//                    if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-//                        // File found - data retrieved
-//                        if let fileContent = try? String(contentsOf: fileURL) {
-//                            // Since this is empty - nothing will load
-//                        }
-//                    }
-//                }
-                    
+                // List
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+                            
             }
-            .navigationTitle(Text("List View"))
+            .navigationTitle(Text("Root Word"))
         }
     }
+    
+    //Methods
+    func addNewWord() {
+        let answer = newWord.lowercased()
+            .trimmingCharacters(in: .whitespaces)
+        
+        // Guards against empty
+        guard answer.count > 0 else {
+            return
+        }
+        
+        // Adds answer to the usedWord array - then reset newWord
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
